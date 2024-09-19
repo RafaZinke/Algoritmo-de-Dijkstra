f727d2b39c66ed38ffb661811fb98d891ec6e504
Rafael Zink <rafael.zink@unidavi.edu.br>
2024-09-19 11:24:53 -0300

    Alterando valores

--- TrabalhoRafael.py
+++ TrabalhoRafael.py
@@ -3,7 +3,7 @@ from tkinter import simpledialog
 import networkx as nx
 
 
-def create_graph():
+def criaGrafo():
     G = nx.Graph()
     G.add_edge('Rio do Sul', 'Presidente Getulio', weight=66)
     G.add_edge('Rio do Sul', 'Ibirama', weight=22.5)
@@ -15,19 +15,19 @@ def create_graph():
 
     return G
 
-def calculate_path(graph, start, end):
+def calculaCaminho(graph, start, end):
     path = nx.dijkstra_path(graph, source=start, target=end, weight='weight')
     cost = nx.dijkstra_path_length(graph, source=start, target=end, weight='weight')
     return path, cost
 
 def main():
     root = tk.Tk()
-    graph = create_graph()
+    graph = criaGrafo()
 
     while True:
         start = simpledialog.askstring("Input", "Diga a cidade Inicial:", parent=root)
         end = simpledialog.askstring("Input", "Diga a cidade final:", parent=root)
-        path, cost = calculate_path(graph, start, end)
+        path, cost = calculaCaminho(graph, start, end)
         
         result_text = f"O caminho mais curto {start} para {end} é {path} Com um custo de: {cost}."
         tk.messagebox.showinfo("Resultado:", result_text)
